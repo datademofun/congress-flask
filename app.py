5b790da65da3d87dfc0cd52b93f486d85ae83d1a
Dan Nguyen <dansonguyen@gmail.com>
2016-04-27 09:33:44 -0700

    basic!

--- app.py
+++ app.py
@@ -2,14 +2,10 @@ from flask import render_template, Flask
 from datafoo import congress
 
 app = Flask(__name__)
-congressmembers = congress.get_legislators()
-
-
 
 @app.route('/')
 def homepage():
-    oldtoyoung = sorted(congressmembers, key=lambda x: x['bio']['birthday'])
-    html = render_template('homepage.html', legislators=oldtoyoung)
+    html = render_template('homepage.html')
     return html
 
 
@@ -17,3 +13,16 @@ if __name__ == '__main__':
     app.run(use_reloader=True, debug=True)
 
 
+
+
+# from datafoo import congress
+
+# app = Flask(__name__)
+
+
+# congressmembers = congress.get_legislators()
+# @app.route('/')
+# def homepage():
+#     oldtoyoung = sorted(congressmembers, key=lambda x: x['bio']['birthday'])
+#     html = render_template('homepage.html', legislators=oldtoyoung)
+#     return html
